9fc16a95bba7f176ff3231176f38137ba083bed0
Your Name <you@example.com>
2021-10-05 05:27:39 +0300

    model product and category

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class HomeComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
         return view('livewire.home-component')->layout('layouts.base');
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Admin\AdminDashboerdComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboerdComponent;
@@ -23,12 +24,7 @@ Route::get('/', HomeComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
-
-/* Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
- */
-/* For User Dashboard  */
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('user/dashboard', UserDashboerdComponent::class)->name('user.dashboard');
 });
